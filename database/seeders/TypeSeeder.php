3c6367c6bd26680d519665be437125f963a80439
Anymoose98 <daniel.digiacomo98@gmail.com>
2024-03-02 17:18:51 +0100

    Creato e inserito dati all'interno della table type

--- database/seeders/TypeSeeder.php
+++ database/seeders/TypeSeeder.php
@@ -2,9 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\Type;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-
 class TypeSeeder extends Seeder
 {
     /**
@@ -12,8 +12,16 @@ class TypeSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run() {
+        // Creo un array di categorie
+        $names = ['Azione', 'Avventura', 'Scienza', 'Fantasy', 'Mistri'];
     {
-        //
+        // lo ciclo e inserisco nel database
+        foreach ($names as $name) {
+            $type = new Type();
+            $type->type = $name;
+            $type->save();
+        }
     }
+}
 }
